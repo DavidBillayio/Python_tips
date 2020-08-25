90e0b933ab2bbcef90d40bd45721d5f0cb4eaafd
David Billay <davidbillay1@gmail.com>
2020-08-25 09:32:21 -0600

    Signed-off-by: David Billay <davidbillay1@gmail.com>

---
+++ Python_tips.py
@@ -0,0 +1,12 @@
+#Python 3 Tips
+
+print(5 + 5) # Addition
+print(5 - 5) #subtraction
+
+
+print(3 * 5) # Multiplication,
+print(10 / 2) #division, 
+print(18 % 7) #modulo
+print(4 ** 2) #exponentiation
+
+print(100 * 1.1 ** 7) #basic interest calculation
